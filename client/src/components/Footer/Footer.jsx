a318601c14f270f60d32e46879331d62616d13d3
KatieBullock <kathrynleahbullock@gmail.com>
2021-05-21 13:03:19 -0400

    designed UI for nav and footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,56 +1,48 @@
 import { Link } from "react-router-dom";
-import { Box, Stack, ButtonGroup, Button, IconButton } from "@chakra-ui/react";
-import { Text } from "@chakra-ui/layout";
+import { Flex, Stack, Button, IconButton, Text } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <Box
-      as="footer"
-      role="contentinfo"
-      mx="auto"
-      maxW="7xl"
-      py="12"
-      px={{ base: "4", md: "8" }}
+    <Flex
+      maxW={"6xl"}
+      p={4}
+      direction={{ base: "column", md: "row" }}
+      spacing={4}
+      justify={{ base: "center", md: "space-between" }}
+      align={{ base: "center", md: "center" }}
     >
-      <Stack>
-        <Stack
-          direction="row"
-          spacing="4"
-          align="center"
-          justify="space-between"
+      <Link to="/bookshelf">
+        <Button
+          leftIcon={<FontAwesomeIcon icon={faBookmark} />}
+          aria-label="Bookshelf"
+          variant="ghost"
         >
-          <Link to="/bookshelf">
-            <Button
-              variant="ghost"
-              color="gray.600"
-              leftIcon={<FontAwesomeIcon icon={faBookmark} size="lg" />}
-            >
-              Bookshelf
-            </Button>
-          </Link>
-          <ButtonGroup variant="ghost" color="gray.600">
-            <IconButton
-              as="a"
-              href="https://www.linkedin.com/in/klbullock/"
-              aria-label="LinkedIn"
-              icon={<FontAwesomeIcon icon={faLinkedin} size="lg" />}
-            />
-            <IconButton
-              as="a"
-              href="https://github.com/KatieBullock/bookshelf-app"
-              aria-label="GitHub"
-              icon={<FontAwesomeIcon icon={faGithubSquare} size="lg" />}
-            />
-          </ButtonGroup>
-        </Stack>
-        <Text fontSize="sm">
-          &copy; {new Date().getFullYear()} Katie Bullock. All rights reserved.
-        </Text>
+          Bookshelf
+        </Button>
+      </Link>
+      <Text>
+        &copy; {new Date().getFullYear()} Katie Bullock. All rights reserved.
+      </Text>
+      <Stack direction={"row"} spacing={2}>
+        <IconButton
+          as="a"
+          href="https://www.linkedin.com/in/klbullock/"
+          icon={<FontAwesomeIcon icon={faLinkedin} size="lg" />}
+          aria-label="LinkedIn"
+          variant="ghost"
+        />
+        <IconButton
+          as="a"
+          href="https://github.com/KatieBullock/bookshelf-app"
+          icon={<FontAwesomeIcon icon={faGithubSquare} size="lg" />}
+          aria-label="GitHub"
+          variant="ghost"
+        />
       </Stack>
-    </Box>
+    </Flex>
   );
 };
 
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,69 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Flex, HStack, IconButton, Button } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBookmark,
+  faSearch,
+  faSignOutAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import { AccessTokenContext } from "../../context/AccessTokenContext";
 
 const Navbar = () => {
+  const { logout } = useContext(AccessTokenContext);
+
   return (
-    <div>
-      <Link to="/bookshelf">Bookshelf</Link>
-      <Link to="/search">Search</Link>
-    </div>
+    <Flex h={100} px={4} alignItems={"center"} justifyContent={"space-between"}>
+      <HStack spacing={4} display={{ md: "none" }}>
+        <Link to="/search">
+          <IconButton
+            icon={<FontAwesomeIcon icon={faSearch} />}
+            aria-label={"Search"}
+            variant="ghost"
+          />
+        </Link>
+      </HStack>
+      <HStack spacing={8} alignItems={"center"}>
+        <Link to="/bookshelf">
+          <Button
+            leftIcon={<FontAwesomeIcon icon={faBookmark} />}
+            aria-label={"Bookshelf"}
+            variant="ghost"
+          >
+            Bookshelf
+          </Button>
+        </Link>
+        <HStack spacing={4} display={{ base: "none", md: "flex" }}>
+          <Link to="/search">
+            <Button
+              leftIcon={<FontAwesomeIcon icon={faSearch} />}
+              aria-label={"Search"}
+              variant="ghost"
+            >
+              Search
+            </Button>
+          </Link>
+        </HStack>
+      </HStack>
+      <HStack spacing={4} display={{ md: "none" }}>
+        <IconButton
+          icon={<FontAwesomeIcon icon={faSignOutAlt} />}
+          aria-label={"Logout"}
+          variant="ghost"
+          onClick={logout}
+        />
+      </HStack>
+      <HStack spacing={4} display={{ base: "none", md: "flex" }}>
+        <Button
+          leftIcon={<FontAwesomeIcon icon={faSignOutAlt} />}
+          aria-label={"Logout"}
+          variant="ghost"
+          onClick={logout}
+        >
+          Logout
+        </Button>
+      </HStack>
+    </Flex>
   );
 };
 
